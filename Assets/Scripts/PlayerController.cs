7f3f1a074cb5cf75983d38b8af698d1c5f41c671
mandel cohen <mandel.cohen@student.forsbergsskola.se>
2023-10-04 15:58:35 +0200

    Update PlayerController Script

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -15,6 +15,8 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        bool isKeyDown = Input.GetKeyDown(KeyCode.Escape);
+        
         transform.Translate(0, 0,speed);
     }
 }
