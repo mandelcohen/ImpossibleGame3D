bf0ea8268794afa50ddc51df51281109b99dcb0d
mandel cohen <mandel.cohen@student.forsbergsskola.se>
2023-10-05 10:12:03 +0200

    Add mesh script

---
+++ Assets/Scripts/MeshFix.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+namespace DefaultNamespace
+{
+    public class MeshFix : MonoBehaviour
+    {
+        [ExecuteInEditMode]
+        public class DynamicCubeMesh : MonoBehaviour
+        {
+            private Vector3 _currentScale;
+        }
+    }
+}
