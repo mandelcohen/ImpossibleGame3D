fa89f7c356da7ef21305ff85b42a9fa6e3adda1f
mandel cohen <mandel.cohen@student.forsbergsskola.se>
2023-10-05 13:54:22 +0200

    Change win scene to lunar

---
+++ Assets/Scripts/StartGameButton.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+namespace DefaultNamespace
+{
+    public class StartGameButton : MonoBehaviour
+    {
+        public void HoverStart()
+        {
+            
+        }
+        
+        public void HoverEnd()
+        {
+            
+        }
+        
+        public void ClickStart()
+        {
+            
+        }
+        public void ClickEnd()
+        {
+            
+        }
+
+        public void Click()
+        {
+            
+        }
+    }   
+}
